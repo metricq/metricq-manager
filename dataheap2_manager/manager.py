06a09e7760870b3908d1047b1411375d58db844e
tilsche <thomas.ilsche@tu-dresden.de>
2018-03-15 18:10:03 +0100

    futile attempt to get rid of the reset by peer

--- dataheap2_manager/manager.py
+++ dataheap2_manager/manager.py
@@ -23,10 +23,14 @@ class Manager:
         logger.info("establishing rpc connection to {}", rpc_url)
         self.rpc_connection = pika.BlockingConnection(pika.URLParameters(rpc_url))
         self.rpc_channel = self.rpc_connection.channel()
+        # Try to get rid of the nasty peer resetting our connections
+        self.rpc_channel.basic_qos(prefetch_count=1)
 
         logger.info("establishing data connection to {}", data_url)
         self.data_connection = pika.BlockingConnection(pika.URLParameters(data_url))
         self.data_channel = self.data_connection.channel()
+        # Try to get rid of the nasty peer resetting our connections
+        self.data_channel.basic_qos(prefetch_count=1)
 
         logger.info("subscribing to rpc queue: {}", rpc_queue)
         self.rpc_channel.queue_declare(queue=rpc_queue)
@@ -54,7 +58,8 @@ class Manager:
 
     def handle_rpc(self, channel, method, properties, body):
         token = properties.app_id
-
+        if isinstance(body, bytes):
+            body = body.decode()
         rpc = json.loads(body)
         logger.info('recieved {} from {}', rpc, token)
 
@@ -118,5 +123,9 @@ def validate_url(ctx, param, value):
 @click.option('--config-path', default='.', type=click.Path(exists=True, file_okay=False, dir_okay=True))
 @click_log.simple_verbosity_option(logger)
 def manager(rpc_url, data_url, rpc_queue, data_queue, data_exchange, config_path):
-    m = Manager(rpc_url, data_url, rpc_queue, data_queue, data_exchange, config_path)
-    m.run()
+    while True:
+        try:
+            m = Manager(rpc_url, data_url, rpc_queue, data_queue, data_exchange, config_path)
+            m.run()
+        except pika.exceptions.ConnectionClosed:
+            pass
