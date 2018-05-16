e21f5bc9b6b974614d188f4e17d54f868cf8702f
tilsche <thomas.ilsche@tu-dresden.de>
2018-05-16 19:47:05 +0200

    Bind only selected metrics to db queue

--- dataheap2_manager/manager.py
+++ dataheap2_manager/manager.py
@@ -142,8 +142,9 @@ class Manager(Agent):
         data_queue = await self.data_channel.declare_queue(data_queue_name)
         logger.debug('declared queue {} for {}', data_queue, from_token)
 
-        await history_queue.bind(exchange=self.history_exchange, routing_key="#")
-        await data_queue.bind(exchange=self.data_exchange, routing_key="#")
+        for metric in self.read_config(from_token)['metrics']:
+            await history_queue.bind(exchange=self.history_exchange, routing_key=metric['name'])
+            await data_queue.bind(exchange=self.data_exchange, routing_key=metric['name'])
 
         response = {
                    "dataServerAddress": self.data_url,
