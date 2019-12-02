e7fcc5e2947ff07e5bad0cb5c98e074e72a03ab2
Franz Hoepfner <franz.hoepfner@tu-dresden.de>
2019-12-02 09:49:20 +0100

    Move get_metrics logic to baseclass

--- metricq_manager/manager.py
+++ metricq_manager/manager.py
@@ -525,91 +525,10 @@ class Manager(ManagementAgent):
         limit=None,
         **body,
     ):
-        if format not in ("array", "object"):
-            raise AttributeError("unknown format requested: {}".format(format))
 
-        if infix is not None and prefix is not None:
-            raise AttributeError('cannot get_metrics with both "prefix" and "infix"')
-
-        selector_dict = dict()
-        if selector is not None:
-            if isinstance(selector, str):
-                selector_dict["_id"] = {"$regex": selector}
-            elif isinstance(selector, list):
-                if len(selector) < 1:
-                    raise ValueError("Empty selector list")
-                if len(selector) == 1:
-                    # That may possibly be faster.
-                    selector_dict["_id"] = selector[0]
-                else:
-                    selector_dict["_id"] = {"$in": selector}
-            else:
-                raise TypeError(
-                    "Invalid selector type: {}, supported: str, list", type(selector)
-                )
-        if historic is not None:
-            if not isinstance(historic, bool):
-                raise AttributeError(
-                    'invalid type for "historic" argument: should be bool, is {}'.format(
-                        type(historic)
-                    )
-                )
-
-        # TODO can this be unified without compromising performance?
-        # Does this even perform well?
-        # ALSO: Async :-[
-        if selector_dict:
-            if historic is not None:
-                selector_dict["historic"] = historic
-            if prefix is not None or infix is not None:
-                raise AttributeError(
-                    'cannot get_metrics with both "selector" and "prefix" or "infix".'
-                )
-            aiter = self.couchdb_db_metadata.find(selector_dict, limit=limit)
-            if format == "array":
-                metrics = [doc["_id"] async for doc in aiter]
-            elif format == "object":
-                metrics = {doc["_id"]: doc.data async for doc in aiter}
-
-        else:  # No selector dict, all *fix / historic filtering
-            request_limit = limit
-            if infix is None:
-                request_prefix = prefix
-                if historic is not None:
-                    endpoint = self.couchdb_db_metadata.view("index", "historic")
-                else:
-                    endpoint = self.couchdb_db_metadata.all_docs
-            else:
-                request_prefix = infix
-                # These views produce stupid duplicates thus we must filter ourselves and request more
-                # to get enough results. We assume for no more than 6 infix segments on average
-                if limit is not None:
-                    request_limit = 6 * limit
-                if historic is not None:
-                    endpoint = self.couchdb_db_metadata.view("components", "historic")
-                else:
-                    raise NotImplementedError(
-                        "non-historic infix lookup not yet supported"
-                    )
-            if format == "array":
-                metrics = [
-                    key
-                    async for key in endpoint.ids(
-                        prefix=request_prefix, limit=request_limit
-                    )
-                ]
-                if request_limit != limit:
-                    # Object of type islice is not JSON serializable m(
-                    metrics = list(islice(sorted(set(metrics)), limit))
-            elif format == "object":
-                metrics = {
-                    doc["_id"]: doc.data
-                    async for doc in endpoint.docs(
-                        prefix=request_prefix, limit=request_limit
-                    )
-                }
-                if request_limit != limit:
-                    metrics = dict(islice(sorted(metrics.items()), limit))
+        metrics = await self.get_metrics(
+            format, historic, selector, prefix, infix, limit
+        )
 
         return {"metrics": metrics}
 
